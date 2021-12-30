099e82b75fbbe9d7ecba09eaa568967aa6783138
rony thankachan <ronythankachan@ronys-MacBook-Air.local>
2021-12-30 17:55:45 +0530

    instagram clone header

---
+++ components/Header.js
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import {
+  SearchIcon,
+  HomeIcon,
+  MenuIcon,
+  PaperAirplaneIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <div>
+      <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
+        <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        {/* middle search*/}
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+        {/* right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="w-10 h-10" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          <PaperAirplaneIcon className="h-10 w-10" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
