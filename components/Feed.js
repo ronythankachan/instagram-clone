d0ca2dd8abd39fd980b6f3506ed50d6b9c138fb9
rony thankachan <rony.mail2me@gmail.com>
2022-01-02 20:58:46 +0530

    sidemenu styling corrected

--- components/Feed.js
+++ components/Feed.js
@@ -12,9 +12,11 @@ function Feed() {
         <Posts />
         {/* posts */}
       </section>
-      <section>
-        <MiniProfile />
-        <Suggestions />
+      <section className="hidden xl:inline-grid md:col-span-1">
+        <div className="fixed">
+          <MiniProfile />
+          <Suggestions />
+        </div>
       </section>
     </main>
   );
